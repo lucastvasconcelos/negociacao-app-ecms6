62be9098a34f42c379be4926c9557233a4c9f764
Lucas Tavares Vasconcelos <lucastvasconcelos@users.noreply.github.com>
2017-06-09 19:27:21 -0300

    lista de negociacoes implementada e devidamente encapsulada

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,16 +4,28 @@ class NegociacaoController {
         this._inputData = $("#data")
         this._inputQuant = $("#quantidade")
         this._inputValor = $("#valor")
-
+        this._listaNegociacao = new ListaNegociacoes()
     }        
     adiciona(event){
         event.preventDefault()
-        let negociacao = new Negociacao(
+        this._listaNegociacao.adiciona(this._criaNegociacao())
+        this._limpaFormulario()
+        console.log(this._listaNegociacao)
+        this._listaNegociacao.negociacoes.length = 0
+        console.log(this._listaNegociacao)
+         }
+
+    _criaNegociacao(){
+        return new Negociacao(
             DataHelper.textoParaData(this._inputData.value),
             this._inputQuant.value,
             this._inputValor.value
         )
-        console.log(negociacao.data)
-        console.log(DataHelper.dataParaTexto(negociacao.data))   
+    }
+
+    _limpaFormulario(){
+        let form = document.querySelector(".form")
+        form.reset()
+        this._inputData.focus()
     }
 }
