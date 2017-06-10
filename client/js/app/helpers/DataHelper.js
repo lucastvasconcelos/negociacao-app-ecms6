740d6f2b2c2103f4e6585fe0887e1582f3e9328a
lucastvasconcelos <lucastavaresvasconcelos@gmail.com>
2017-06-10 14:31:31 -0700

    tabela negociacoes ja implementada

--- client/js/app/helpers/DataHelper.js
+++ client/js/app/helpers/DataHelper.js
@@ -3,6 +3,6 @@ class DataHelper{
         return new Date(...data.split('-').map((item,indice) => item - indice%2 ))
     }
     static dataParaTexto(data){
-        return `${data.getDate()}/${data.getMonth()}/${data.getFullYear()}`
+        return `${data.getDate()}/${(data.getMonth()+1)}/${data.getFullYear()}`
     }
 }
--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -9,24 +9,37 @@ class NegociacoesView {
 	        <thead>
 	            <tr>
 	                <th>DATA</th>
-	                <th>QUANTIDADE</th>s
+	                <th>QUANTIDADE</th>
 	                <th>VALOR</th>
 	                <th>VOLUME</th>
 	            </tr>
 	        </thead>
 	        
 	        <tbody>
-				${model.negociacoes.map((n) => {
-					console.log(n)	
-				})}
+				${model.negociacoes.map((negociacao) => 
+							`
+								<tr>
+									<td>${DataHelper.dataParaTexto(negociacao.data)}</td>
+									<td>${negociacao.quantidade}</td>
+									<td>${negociacao.valor}</td>
+									<td>${negociacao.obtemVolume()}</td>								
+								</tr>
+							`
+				).join("")}
 	        </tbody>
 	        
 	        <tfoot>
-	        </tfoot>
+				<td colspan="3"></td>
+				<td>
+				${
+					model.negociacoes.reduce((total,negociacao) => total += negociacao.obtemVolume(),0.0)
+				}	
+				</td>
+			</tfoot>
 	    </table>
 	    `
 	}
-
+		
 	_update(model){
 		console.log(model)
 		this._element.innerHTML = this._template(model)
